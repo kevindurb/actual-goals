e4842c41cdbbb8037f56b009aff7caf4e058ece4
Kevin Durbin <kevindurb@fastmail.com>
2025-01-15 15:47:34 -0700

    fix type import

--- public/goals/goals-list.ts
+++ public/goals/goals-list.ts
@@ -1,57 +1,57 @@
-import { css, html, LitElement } from 'lit';
-import { customElement } from 'lit/decorators.js';
-import { Router } from '@lit-labs/router';
-import { GoalsController, type Goal } from './goals-controller.ts';
+import type { Router } from '@lit-labs/router';
 import { consume } from '@lit/context';
+import { LitElement, css, html } from 'lit';
+import { customElement } from 'lit/decorators.js';
 import { routerContext } from '../router-context.ts';
+import { type Goal, GoalsController } from './goals-controller.ts';
 
 @customElement('goals-list')
 export class GoalsList extends LitElement {
-  static override styles = [
-    css`
+	static override styles = [
+		css`
       md-fab {
         position: fixed;
         bottom: var(--md-sys-spacing-md);
         right: var(--md-sys-spacing-md);
       }
     `,
-  ];
+	];
 
-  @consume({ context: routerContext })
-  private router!: Router;
+	@consume({ context: routerContext })
+	private router!: Router;
 
-  private controller = new GoalsController(this);
+	private controller = new GoalsController(this);
 
-  private renderGoalList(goals: Goal[]) {
-    return goals.map(
-      (goal) => html`
+	private renderGoalList(goals: Goal[]) {
+		return goals.map(
+			(goal) => html`
         <md-list-item @click=${() => this.router.goto(`/goals/${goal.id}`)}</md-list-item>>
           <md-circular-progress slot="start" value="0.6"></md-circular-progress>
           <div slot="headline">${goal.name}</div>
           <div slot="supporting-text">
             ${
-              goal.type === 'ONE_TIME'
-                ? `${goal.amount} by ${goal.end}`
-                : `${goal.amount} every month`
-            }
+							goal.type === 'ONE_TIME'
+								? `${goal.amount} by ${goal.end}`
+								: `${goal.amount} every month`
+						}
           </div>
         </md-list-item>
         <md-divider inset></md-divider>
       `,
-    );
-  }
+		);
+	}
 
-  override render() {
-    return html`
+	override render() {
+		return html`
       <ag-header center>Goals</ag-header>
       <md-list>
         ${this.controller.listTask.render({
-          complete: (goals) => this.renderGoalList(goals),
-        })}
+					complete: (goals) => this.renderGoalList(goals),
+				})}
       </md-list>
       <md-fab label="New" @click=${() => this.router.goto('/goals/new')}>
         <md-icon slot="icon">add</md-icon>
       </md-fab>
     `;
-  }
+	}
 }
