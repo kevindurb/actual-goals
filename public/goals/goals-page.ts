bc06779347fc28d2566e2ed3995ab11f3399c575
Kevin Durbin <kevindurb@fastmail.com>
2025-01-14 19:52:43 -0700

    get fonts working

--- public/goals/goals-page.ts
+++ public/goals/goals-page.ts
@@ -1,10 +1,12 @@
 import { html, LitElement } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import { Task } from '@lit/task';
+import { styles as typescaleStyles } from '@material/web/typography/md-typescale-styles.js';
 import type { Goal } from '../../src/GoalModel.ts';
 
 @customElement('goals-page')
 export class GoalsPage extends LitElement {
+  static override styles = [typescaleStyles.styleSheet!];
   #goalsTask = new Task<[unknown], Goal[]>(this, {
     task: () => fetch('/api/goals').then((res) => res.json()),
     args: () => [this.#deleteGoal.value],
@@ -29,7 +31,7 @@ export class GoalsPage extends LitElement {
 
   override render() {
     return html`
-      <h1>Goals</h1>
+      <h1 class="md-typescale-display-large">Goals</h1>
       <md-list>
         ${this.#goalsTask.render({
           complete: (goals) => this.#renderGoalList(goals),
@@ -39,7 +41,7 @@ export class GoalsPage extends LitElement {
         @click=${() => {
           window.location.href = '/goals/new';
         }}
-        ><md-icon slot="icon">add</md-icon></md-fab
+        ><md-icon slot="icon">edit</md-icon></md-fab
       >
     `;
   }
--- public/main.ts
+++ public/main.ts
@@ -1,5 +1,9 @@
 import { URLPattern } from 'urlpattern-polyfill';
+import '@material/web/all.js';
+import { styles as typescaleStyles } from '@material/web/typography/md-typescale-styles.js';
+
 import './actual-goals-app.js';
 
 // @ts-ignore: Property 'UrlPattern' does not exist
 globalThis.URLPattern = URLPattern;
+document.adoptedStyleSheets.push(typescaleStyles.styleSheet!);
